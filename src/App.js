d97edb57f5b590420aaf9d1b04fdc9e6dda75296
Dimkasss <spharde.god@gmail.com>
2022-10-24 21:30:00 +0400

    hooks implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import './Button'
 import Button from './Button';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { editableInputTypes } from '@testing-library/user-event/dist/utils';
 
 const initialState = {
     word1: "",
@@ -14,76 +14,90 @@ const initialState = {
   }
 
 
-class App extends React.Component {
+const App = () => {
+	const [words, setWords] = useState({
+		word1: "",
+		word2: ""
+	});
+	const [key, setKey] = useState(0);
+	const [answered, setAnswered] = useState(false);
+	const [correct, setCorrect] = useState(false);
+	const [clicked, setClicked] = useState(0);
 
-  constructor () {
-    super();
-    this.state = initialState
-  }
-  componentDidMount() {
-    this.task();
-  }
-  task = () => {
-    this.setState(initialState)
-    let requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("http://localhost:3000/words", requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({word1: data.words[0]});
-        this.setState({word2: data.words[1]});
-        this.setState({key: data.whichOne});
-      })
-      .catch(error => console.log('error', error));
-  }
+	useEffect(() => {
+		task();
+	}, [])
 
-  pick = (n) => {
-    this.setState({answered: true});
-    this.setState({clicked: n})
-    if(this.state.key == n){
-      this.setState({correct: true})
-    }
-    else{
-      this.setState({correct: false})
-    }
-  }
+	useEffect(() => {
 
+	}, [answered])
+	
+  	const task = () => {
+		setWords({word1: "", word2: ""})
+		setKey(0);
+		setAnswered(false);
+		setCorrect(false);
+		setClicked(0);
+  		let requestOptions = {
+			method: 'GET',
+			redirect: 'follow'
+  		};
+		
+  		fetch("http://localhost:3000/words", requestOptions)
+  	    .then(response => response.json())
+  	    .then(data => {
+  	    	setWords({
+				word1: data.words[0],
+				word2: data.words[1],
+			})
+			setKey(data.whichOne)
 
-  render() {
-    const answeredOpacity = {
-      opacity: this.state.answered ? 1 : 0,
-      pointerEvents: this.state.answered ? 'all' : 'none'
-    }
-    const answeredOptions = {
-      pointerEvents: this.state.answered ? 'none' : 'all'
-    }
+  	    })
+  	    .catch(error => console.log('error', error));
+  	}
 
-    return (
-    <div className="App">
-      <div className='cont'>
-        <div className="task__name">
-          Задание 4
-        </div>
-        <div className="task__desc">
-          Выберите слово, где <b>верно</b> поставлено ударение:
-        </div>
-        <div className="task__cont" style={answeredOptions}>
-          <Button value={this.state.word1} color={this.state.answered ? (this.state.clicked ? 'purple' : (this.state.correct ? 'green' : 'red')) : 'purple'} onClick={() => this.pick(0)}/>
-          <Button value={this.state.word2} color={this.state.answered ? (this.state.clicked ? (this.state.correct ? 'green' : 'red') : 'purple') : 'purple'} onClick={() => this.pick(1)}/>
-        </div>
-        <div className="task__check" style={answeredOpacity}>
-          <div className="task__check__clearness">
-            {this.state.correct ? 'Верно!! УраУра' : (`Неверно!! Правильный вариант: ` + (this.state.key ? this.state.word2 : this.state.word1))}
-          </div>
-          <Button value="Продолжить" color="lightblue" onClick={() => this.task()}/>
-        </div>
-      </div>
-    </div>
-  );
-  } 
+  	const pick = (n) => {
+  		setAnswered(true);
+		setClicked(n);
+  		if(key == n){
+  			setCorrect(true)
+  		}
+  		else {
+  			setCorrect(false)
+  		}
+  	}
+
+  	const answeredOpacity = {
+  	  opacity: answered ? 1 : 0,
+  	  pointerEvents: answered ? 'all' : 'none'
+  	}
+  	const answeredOptions = {
+  	  pointerEvents: answered ? 'none' : 'all'
+  	}
+
+  	return (
+  	<div className="App">
+  		<div className='cont'>
+			<div className="task__name">
+				Задание 4
+			</div>
+			<div className="task__desc">
+				Выберите слово, где <b>верно</b> поставлено ударение:
+			</div>
+			<div className="task__cont" style={answeredOptions}>
+				<Button value={words.word1} color={answered ? (clicked ? 'purple' : (correct ? 'green' : 'red')) : 'purple'} onClick={() => pick(0)}/>
+				<Button value={words.word2} color={answered ? (clicked ? (correct ? 'green' : 'red') : 'purple') : 'purple'} onClick={() => pick(1)}/>
+			</div>
+			<div className="task__check" style={answeredOpacity}>
+				<div className="task__check__clearness">
+					{correct ? 'Верно!! УраУра' : (`Неверно!! Правильный вариант: ` + (key ? words.word2 : words.word1))}
+				</div>
+				<Button value="Продолжить" color="lightblue" onClick={() => task()}/>
+			</div>
+  		</div>
+  	</div>
+  	);
 }
+
 
 export default App;
